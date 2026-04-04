32e185399077960d839afe38fc520be8d32e1068
matheusln-web <matheusln1848@gmail.com>
2026-04-04 22:36:23 +0000

    Update 1.0 Estudo de logica: Nesse primeiro estudo, vi como: Mostrar uma mensagem na tela e depois como declarar variaveis e seus tipos.

---
+++ estudo_variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+    int main(){
+        // int, float, chat, são tipos de dados primitivos.
+        int idade;// pode usar letras normalmente.
+        // int -> numeros inteiros.
+        int Idade;// mesmo o nome da variavel sendo o mesmo, por haver uma alteração de i > I, é considerado outra variavel.
+        float altura_2 = 1.65;// não pode usar espaços, porem pode simbolizar com _.
+        // float -> valores com virgula (9.2 1.234)
+        double peso = 90.3;
+        // mesma função do float, porem podendo trabalhar com mais caracteres.
+        char nome = 'A';
+        // char -> trabalhar com caracteres (l, m, a, "5").
+        char nome[20] = "sergio";
+        // especifica o tamanho que vou trabalhar.
+        
+altura_2 = 1.87; //alteração do valor da variavel já inicializada
+
+
+    }
