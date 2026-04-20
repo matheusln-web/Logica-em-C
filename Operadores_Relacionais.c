34f1e001ab96dfb4574c3ce9064230d5ce9030ee
matheusln-web <matheusln1848@gmail.com>
2026-04-20 13:37:21 +0000

    Estudo dos operadores relacionais

---
+++ Operadores_Relacionais.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+/*    
+    int a = 20;
+    int b = 20;
+
+    printf("a > b: %d\n", a >= b);
+    printf("a < b: %d\n", a <= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+*/
+/*    
+    int x = 5;
+    float y = 5.0;
+
+    printf("x >= y: %d\n", x >= y);
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+*/
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+    printf("numero1 > numero2: %d\n", (int)numero1 > numero2);//forcei a comparação dos valores mesmo em tipagens diferentes.
+    printf("numero1 == numero2: %d\n", (int)numero1 == numero2);
+
+    return 0;
+
+}
+
